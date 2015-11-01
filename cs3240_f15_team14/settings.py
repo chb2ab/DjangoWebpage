96c389fe906a92bfec66bfc30ba65ad6dfec892c
ahy9ng <ahy9ng@virginia.edu>
2015-11-01 04:57:12 -0500

    sample login page without its auth functionality, will update visuals later with polymer

--- cs3240_f15_team14/settings.py
+++ cs3240_f15_team14/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'cs3240_f15_team14.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
